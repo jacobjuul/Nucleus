2fac18e2524c4f21a52404ed0791b3ca998c5282
Jacob Juul Christiansen <jacob.juul@gmail.com>
2017-06-16 13:52:46 +0900

    remove console.log

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -5,7 +5,6 @@ import api from '../utils/apiHelper';
 export const fetchPosts = () => (dispatch) => {
   dispatch({ type: types.FETCH_POSTS });
   api.posts().then((content, error) => {
-    console.log(content.hits);
     dispatch({
       type: error ? types.FETCH_POSTS_FAILURE : types.FETCH_POSTS_SUCCESS,
       posts: !error && content.hits,
