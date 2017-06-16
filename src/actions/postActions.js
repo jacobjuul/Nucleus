b8a64e61a7bc8976602639753f17f12bb9d71a2d
Jacob Juul Christiansen <jacob.juul@gmail.com>
2017-06-16 13:02:56 +0900

    fetch all posts when mounting feedlist

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,13 +1,16 @@
 import axios from 'axios';
 import * as types from '../constants/actionTypes';
+import api from '../utils/apiHelper';
 
-export const fetchPosts = () => dispatch => {
+export const fetchPosts = () => (dispatch) => {
   dispatch({ type: types.FETCH_POSTS });
-  axios.get('http://localhost:3000/posts')
-  .then(({ data }) => {
-    dispatch({ type: types.FETCH_POSTS_SUCCESS, posts: data });
-  })
-  .catch((error) => {
-    dispatch({ type: types.FETCH_POSTS_FAILURE, error });
+  api.posts().then((content, error) => {
+    console.log(content.hits);
+    dispatch({
+      type: error ? types.FETCH_POSTS_FAILURE : types.FETCH_POSTS_SUCCESS,
+      posts: !error && content.hits,
+      error
+    });
   });
-}
+};
+
