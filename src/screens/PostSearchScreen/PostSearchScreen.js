930a5e90cf1f8ae59d0fec481481039faa5e8340
Jacob Juul Christiansen <jacob.juul@gmail.com>
2017-06-27 00:25:26 +0900

    open and close the search modal

--- src/screens/PostSearchScreen/PostSearchScreen.js
+++ src/screens/PostSearchScreen/PostSearchScreen.js
@@ -4,24 +4,19 @@ import { View, Text }     from 'react-native'
 import R                  from 'ramda'
 import { navigatorStyle } from '../../constants/styles'
 
-const isButtonPress = R.compose(R.equals('NavBarButtonPress'), R.prop('type'))
-const isClose = R.compose(R.equals('search.close'), R.prop('id'))
+const ifButtonPress = action =>
+  R.when(
+    R.propEq('type', 'NavBarButtonPress'),
+    action
+  )
 
 const PostSearchScreen = ({ navigator }) => {
-
-  const onNavigatorEvent = (event) => {
-    if (event.type === 'NavBarButtonPress') {
-      if (event.id === 'search.close') {
-        navigator.dismissModal()
-      }
-
-      if (event.id === 'search.clear') {
-        // clear input field
-      }
-    }
+  const onNavigatorEvent = event => {
+    if (event.id === 'search.close') navigator.dismissModal()
+    if (event.id === 'search.clear') console.log('clear')
   }
 
-  navigator.setOnNavigatorEvent(onNavigatorEvent)
+  navigator.setOnNavigatorEvent(ifButtonPress(onNavigatorEvent))
   return (
     <View>
       <Text>SearchScreen</Text>
